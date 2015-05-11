bd711704ed84777f2ca478a25aaa12c6e8e35a19
steve wirts <stevewirts@gmail.com>
2015-05-11 16:00:18 -0400

    table integration

--- src/javascript/components/hypergrid.js
+++ src/javascript/components/hypergrid.js
@@ -1,37 +1,42 @@
 var React = require('react'),
-		xIgnite = require('../streams/xignite.js'),
+		ticker = require('../components/ticker.js'),
         _ = require('underscore');
 
 var HyperGrid = React.createClass({
 	componentDidMount: function(){
 
-        // setTimeout(function(){
-        //     var qTree = document.querySelector('#q-example')
-        //     var b = qTree.getBehavior()
-        //     b.setAttribute('url','ws://local:12345')
-        //     b.reconnect();
+        setTimeout(function(){
+            var jsonGrid = document.querySelector('#stock-example')
+            var jsonModel = jsonGrid.getBehavior()
+            
+            jsonModel.setData(ticker.stocks);
+            jsonModel.setFixedColumnCount(1);
+            jsonModel.setFields(['TICKER','High','Low','Last','Change','PercentChange','Volume','Bid','Ask','Spread','BidQuantity','AskQuantity','COUNTRY','ICB','INDUS','SUP_SEC','SEC','SUB_SEC','Date','Time','Open','Close','PreviousClose','PreviousCloseDate','NAME']);
+            
+            var lnfOverrides = {
+                backgroundColor: '#2d2d2d',
+                topLeftBackgroundColor: '#2d2d2d',
+                fixedColumnBackgroundColor: '#2d2d2d',
+                fixedRowBackgroundColor: '#2d2d2d',
+                color: 'lightgrey',
+                topLeftColor: 'lightgrey',
+                fixedColumnColor: 'lightgrey',
+                fixedRowColor: 'lightgrey',
+                lineColor: 'lightgrey',
+            };
 
-        //     var lnfOverrides = {
-        //         backgroundColor: '#2d2d2d',
-        //         topLeftBackgroundColor: '#2d2d2d',
-        //         fixedColumnBackgroundColor: '#2d2d2d',
-        //         fixedRowBackgroundColor: '#2d2d2d',
-        //         color: 'lightgrey',
-        //         topLeftColor: 'lightgrey',
-        //         fixedColumnColor: 'lightgrey',
-        //         fixedRowColor: 'lightgrey',
-        //         lineColor: 'lightgrey',
-        //     };
+            //to apply to a specific table
+            jsonGrid.addProperties(lnfOverrides);
+            setInterval(function() {
+                ticker.randomize();
+                jsonModel.dataModified();
+            }, 100);
 
-        //     //to apply to a specific table
-        //     qTree.addProperties(lnfOverrides);
-
-
-        // },2000);
+        },500);
             
     },
   render: function() {
-    return <div onClick={this.onClick} className="hyper-grid"></div>
+    return <fin-hypergrid id="stock-example"><fin-hypergrid-behavior-json></fin-hypergrid-behavior-json></fin-hypergrid>
   }
 });
 // <fin-hypergrid id="q-example"></fin-hypergrid>
