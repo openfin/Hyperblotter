32f394a8e056c7d383a70d15ca9faac1820c2650
steve wirts <stevewirts@gmail.com>
2015-05-12 09:53:17 -0400

    added formatter

--- src/javascript/components/hypergrid.js
+++ src/javascript/components/hypergrid.js
@@ -1,5 +1,6 @@
 var React = require('react'),
 		ticker = require('../components/ticker.js'),
+        ticker = require('numeral'),
         _ = require('underscore');
 
 var HyperGrid = React.createClass({
@@ -36,6 +37,47 @@ var HyperGrid = React.createClass({
 
             jsonModel.highlightCellOnHover= function(isColumnHovered, isRowHovered) {
                 return isRowHovered;
+            };
+
+            cellProvider.getCell = function(config) {
+                var renderer = cellProvider.cellCache.simpleCellRenderer;
+                config.halign = 'left';
+                var x = config.x;
+                var y = config.y;
+
+                if ((y % 3) === 0 && x < 7){
+                    config.bgColor = '#00ff00';
+                    config.font = 'italic 15px Arial';
+                } else if (((y - 1) % 3) === 0 && x < 7){
+                    config.bgColor = '#0000ff';
+                    config.fgColor = '#ffffff';
+                }
+                if (x === 1) {
+                    renderer = cellProvider.cellCache.linkCellRenderer;
+                } else if (x === 2) {
+                    config.halign = 'center';
+                } else if (x === 3) {
+                    config.halign = 'center';
+                } else if (x === 6) {
+                    config.halign = 'center';
+                } else if (x === 7) {
+                    var travel = 60 + Math.round(config.value*150/100000);
+                    var bcolor = travel.toString(16);
+                    config.halign = 'right';
+                    config.bgColor = '#00' + bcolor + '00';
+                    config.fgColor = '#FFFFFF';
+                    config.value = accounting.formatMoney(config.value);
+                } else if (x === 8) {
+                    var travel = 105 + Math.round(config.value*150/1000);
+                    var bcolor = travel.toString(16);
+                    config.halign = 'right';
+                    config.bgColor = '#' + bcolor+ '0000';
+                    config.fgColor = '#FFFFFF';
+                    config.value = accounting.formatMoney(config.value, "€", 2, ".", ",");
+                }
+
+                renderer.config = config;
+                return renderer;
             };
 
         });
