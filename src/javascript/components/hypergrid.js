0d75cf83c38c70d1eece6661f86d51ef87623187
steve wirts <stevewirts@gmail.com>
2015-05-12 12:08:39 -0400

    fixed up table a bit

--- src/javascript/components/hypergrid.js
+++ src/javascript/components/hypergrid.js
@@ -1,7 +1,38 @@
 var React = require('react'),
 		ticker = require('../components/ticker.js'),
-        ticker = require('numeral'),
+        numeral = require('numeral'),
+        moment = require('moment'),
         _ = require('underscore');
+
+var typeAlignmentMap = {
+    j: 'right',
+    s: 'left',
+    t: 'center',
+    f: 'right',
+    d: 'center'
+};
+
+var isInt = function(n){
+        return Number(n)===n && n%1===0;
+};
+
+var isFloat = function(n){
+        return   n===Number(n)  && n%1!==0
+};
+
+var format = function(data) {
+    if (isInt(data)) {
+        if (data > 1431446226436) { // this is a time
+            return moment(data).format('HH:mm:ss.SSS');
+        } else {
+            return numeral(data).format('0,0');
+        }
+    } else if (isFloat(data)) {
+        return numeral(data).format('0,0.00');
+    } else {
+        return data;
+    }
+};
 
 var HyperGrid = React.createClass({
     componentDidMount: function(){
@@ -13,6 +44,7 @@ var HyperGrid = React.createClass({
 
             jsonModel.setData(ticker.stocks);
             jsonModel.setFixedColumnCount(1);
+            jsonModel.setHeaders(['Symbol','High','Low','Last','Change','% Change','Volume','Bid','Ask','Spread','Bid Quantity','Ask Quantity','Country','ICB','Industry','Super Sector','Sector','Sub Sector','Date','Time','Open','Cls','Previous Cls','Previous Cls Dt','Name']);
             jsonModel.setFields(['TICKER','High','Low','Last','Change','PercentChange','Volume','Bid','Ask','Spread','BidQuantity','AskQuantity','COUNTRY','ICB','INDUS','SUP_SEC','SEC','SUB_SEC','Date','Time','Open','Close','PreviousClose','PreviousCloseDate','NAME']);
             
             var lnfOverrides = {
@@ -50,12 +82,6 @@ var HyperGrid = React.createClass({
                         frame: false
                     })
 
-/*
-
-Ask: 13.155027193983155AskQuantity: 1100Bid: 13.14517905506539BidQuantity: 300COUNTRY: "United States"Change: -0.19082777142769913Close: 0Date: Mon May 11 2015 23:10:20 GMT-0400 (Eastern Daylight Time)High: 27.485ICB: "2727"INDUS: "Industrials"Last: 13.14517905506539Low: 13.14517905506539NAME: "General Electric Co."Open: 27.21PercentChange: -1.4309213688208668PreviousClose: 27.04PreviousCloseDate: Sun May 10 2015 23:10:20 GMT-0400 (Eastern Daylight Time)SEC: "General Industrials"SUB_SEC: "Diversified Industrials"SUP_SEC: "Industrial Goods & Services"Spread: 0.009848138917766392TICKER: "GE"Time: 1431400220319Volume: 1869883
-
-
-                 */
             };
 
             jsonModel.highlightCellOnHover= function(isColumnHovered, isRowHovered) {
@@ -64,45 +90,107 @@ Ask: 13.155027193983155AskQuantity: 1100Bid: 13.14517905506539BidQuantity: 300CO
 
             cellProvider.getCell = function(config) {
                 var renderer = cellProvider.cellCache.simpleCellRenderer;
-                config.halign = 'left';
+                config.halign = 'right';
                 var x = config.x;
                 var y = config.y;
-
-                if ((y % 3) === 0 && x < 7){
-                    config.bgColor = '#00ff00';
-                    config.font = 'italic 15px Arial';
-                } else if (((y - 1) % 3) === 0 && x < 7){
-                    config.bgColor = '#0000ff';
-                    config.fgColor = '#ffffff';
-                }
-                if (x === 1) {
-                    renderer = cellProvider.cellCache.linkCellRenderer;
-                } else if (x === 2) {
-                    config.halign = 'center';
-                } else if (x === 3) {
-                    config.halign = 'center';
-                } else if (x === 6) {
-                    config.halign = 'center';
-                } else if (x === 7) {
-                    var travel = 60 + Math.round(config.value*150/100000);
-                    var bcolor = travel.toString(16);
-                    config.halign = 'right';
-                    config.bgColor = '#00' + bcolor + '00';
-                    config.fgColor = '#FFFFFF';
-                    config.value = accounting.formatMoney(config.value);
-                } else if (x === 8) {
-                    var travel = 105 + Math.round(config.value*150/1000);
-                    var bcolor = travel.toString(16);
-                    config.halign = 'right';
-                    config.bgColor = '#' + bcolor+ '0000';
-                    config.fgColor = '#FFFFFF';
-                    config.value = accounting.formatMoney(config.value, "€", 2, ".", ",");
-                }
-
                 renderer.config = config;
+                config.value = format(config.value);
                 return renderer;
             };
 
+            var state = {  
+               "columnIndexes":[  
+                  0,
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  19
+               ],
+               "fixedColumnIndexes":[  
+
+               ],
+               "hiddenColumns":[  
+                  24,
+                  23,
+                  22,
+                  17,
+                  12,
+                  14,
+                  13,
+                  15,
+                  16,
+                  21,
+                  20,
+                  18
+               ],
+               "columnWidths":[  
+                  null,
+                  66,
+                  64,
+                  55,
+                  55,
+                  65.306640625,
+                  80,
+                  58,
+                  64,
+                  69,
+                  75.115234375,
+                  77.1953125,
+                  101.3935546875,
+                  38.38671875,
+                  126.8603515625,
+                  175.521484375,
+                  null,
+                  null,
+                  null,
+                  null
+               ],
+               "fixedColumnWidths":[  
+                  71
+               ],
+               "rowHeights":{  
+
+               },
+               "fixedRowHeights":{  
+
+               },
+               "sorted":[  
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0
+               ]
+            };
+            jsonModel.setState(state);
         });
             
     },
