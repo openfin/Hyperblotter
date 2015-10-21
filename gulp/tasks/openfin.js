e2b87de570d990af5b999033d5b4008d007a2e0f
mrClapham <info@gcdesign.co.uk>
2015-10-21 13:52:32 +0100

    Gulp task theoretically working but running into Access Denied issues.

--- gulp/tasks/openfin.js
+++ gulp/tasks/openfin.js
@@ -1,11 +1,58 @@
-var gulp   = require('gulp');
+var  gulp   = require('gulp')
+    ,gutil = require("gulp-util")
+    ,exec = require("gulp-exec")
+    ,q = require('q')
+    ,openfinLauncher = require('openfin-launcher')
+    //,openfinConfigBuilder = require('openfin-config-builder')
+    ,nodemon = require('nodemon');
+
+
 var src  = require('../config').js;
 
-var openfinLauncher = require('openfin-launcher'),
+//var openfinLauncher = require('openfin-launcher'),
     path = require('path');
  
+//gulp.task('openfin', function() {
+//  openfinLauncher.launchOpenFin({
+//      configPath: 'file:/' + path.resolve('app.json')
+//  });
+//});
+
+/* Starts up theNode server - returning a promise so it may be chained in the 'openfin' task. */
+function startServer(){
+    var defered = q.defer();
+
+    var _resolve = function(){
+        defered.resolve()
+    }
+
+    nodemon({
+        script: 'server.js'
+        , ext: 'js html'
+        , env: { 'NODE_ENV': 'development' }
+        , events: {
+            "start": _resolve()
+        }
+    });
+    return defered.promise;
+}
+
+function openfinLaunch() {
+    openfinLauncher.launchOpenFin({
+        // Launch a locally hosted Node application.
+        configPath: 'http://localhost:5001/app.json'
+    })
+        .then(function () {
+            console.log('success!');
+        })
+        .fail(function (error) {
+            console.log('error!', error);
+        });
+}
+
+/* THIS IS THE MAIN CALL TO LAUNCH THE OPENFIN APP. */
+
 gulp.task('openfin', function() {
-  openfinLauncher.launchOpenFin({
-      configPath: 'file:/' + path.resolve('app.json')
-  });
+    return startServer()
+            .then(openfinLaunch);
 });
