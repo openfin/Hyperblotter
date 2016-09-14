22f8c5fa710702b5673975aa666770e02736a32c
Raman Sycheuski <raman.sycheuski@gmail.com>
2016-09-14 11:37:23 -0400

    support empty bloomberg

--- src/javascript/components/trade-view.js
+++ src/javascript/components/trade-view.js
@@ -7,14 +7,49 @@ var React = require('react'),
     sub = function(a,b){
       return a - b;
     },
-		rndRange = function () {
+    rndRange = function () {
       return Math.floor(Math.random() * 10 % 5) / 10;
+    },
+    randomPlusMinus = function() {
+        return (Math.round(Math.random() * 10) % 2 === 0) ? '+' : '-';
     },
     plusMinus = function(base, op){
       return parseInt(Math.random() * 10) % 2 ? add(base, op) : sub(base, op);
     };
 
+
 var urlData = location.search.split('&').map((i)=>{return i.split('=')[1]});
+
+var useBloombergData = JSON.parse(urlData[2]);
+require('./bloomberg-plugin-client.js');
+if (useBloombergData) {
+    let serviceId = 1;
+    let requestId = 2;
+
+    let session = new fin.desktop.Plugins.blpapi.Session();
+
+    session.on('SessionStarted', m => {
+        session.openService('//blp/mktdata', serviceId);
+    });
+
+    session.on('ServiceOpened', m => {
+        if (m.correlations[0].value === serviceId) {
+            session.subscribe([{security: 'AAPL US Equity', correlation: requestId, fields: ['LAST_PRICE', 'BID', 'ASK']}]);
+        }
+    });
+
+    session.on('MarketDataEvents', m => {
+        if (m.correlations[0].value === requestId) {
+            console.log(m.data);
+        }
+    });
+
+    session.on('SessionTerminated', m => {
+        session.destroy();
+    });
+
+    session.start();
+}
 
 module.exports = React.createClass({
 	closeWindow: ()=>{
@@ -41,13 +76,23 @@ module.exports = React.createClass({
         console.log("EnterFrame --- ");
     },
     getBackgroundColor:function(){
-        return Math.random() > .5 ? "#ff0000" : "#00DD00";
+        if (useBloombergData) {
+            return '#000';
+        } else {
+            return Math.random() > .5 ? "#ff0000" : "#00DD00";
+        }
     },
     getTileStyle: function(){
 
         return{ "backgroundColor": this.getBackgroundColor()
 
         }
+    },
+    getColorBasedOnPlusMinus: function() {
+        if (!useBloombergData) {
+            return {};
+        }
+        return this.state.plusMinus === '+' ? {color: '#00A500'} : {color: '#FF0000'};
     },
     step: function(timestamp){
         if (!start) start = timestamp;
@@ -94,17 +139,18 @@ module.exports = React.createClass({
   	setInterval(()=>{
   		this.setState({
   			ticker: urlData[0],
-  			last: Number(urlData[1])
+  			last: Number(urlData[1]),
+            plusMinus: randomPlusMinus()
   		});
   	}, 1000 + ( Math.floor(Math.random() * 1000) ) );
   },
     componentWillUnmount:function(){
-        console.log("componentWillUnmount ----- trade-view.js ");
+        // console.log("componentWillUnmount ----- trade-view.js ");
     },
 	render: function(){
-        console.log("RENDERING --- ", this.state.ticker);
+        // console.log("RENDERING --- ", this.state.ticker);
 		return (
-			<div className='tile trade-cell' style={this.getTileStyle()}>
+			<div className={'tile trade-cell' + (useBloombergData ? ' bloomberg' : '')} style={this.getTileStyle()}>
 				<div className="window-control" />
 				<div className="banner">
 					<div className="title">
@@ -114,21 +160,21 @@ module.exports = React.createClass({
 				</div>
 				<div className="content">
 					<div className="main">
-						<span className="last" >{this.state.last.toFixed(2)}</span>
-						<span className="percent-change" >+%{rndRange().toFixed(2)}</span>
+						<span className={"last" + (useBloombergData ? ' bloomberg' : '')} >{this.state.last.toFixed(2)}</span>
+						<span className="percent-change" style={this.getColorBasedOnPlusMinus()}>{useBloombergData ? '' : this.state.plusMinus}%{ useBloombergData ? '' : rndRange().toFixed(2) }</span>
 					</div>
 					<div className="pricing">
 						<div className="price open">
 							<div className="label">OPEN</div>
-							<span className="value">{ (this.state.last - rndRange()).toFixed(2) } </span>
+							<span className="value">{ useBloombergData ? '' : (this.state.last - rndRange()).toFixed(2) } </span>
 						</div>
 						<div className="price high">
 							<div className="label">HIGH</div>
-							<span className="value">{ (this.state.last + rndRange()).toFixed(2) }</span>
+							<span className="value">{ useBloombergData ? '' : (this.state.last + rndRange()).toFixed(2) }</span>
 						</div>
 						<div className="price low">
 							<div className="label">LOW</div>
-							<span className="value">{ (this.state.last - rndRange() - 1).toFixed(2)  }</span>
+							<span className="value">{ useBloombergData ? '' : (this.state.last - rndRange() - 1).toFixed(2)  }</span>
 						</div>
 					</div>
 				</div>
