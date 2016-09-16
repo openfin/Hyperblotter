3199082f3dc66b10308aac788c8aafd1d5e7fb90
Raman Sycheuski <raman.sycheuski@gmail.com>
2016-09-16 12:08:02 -0400

    removed extra code

--- blpapi-plugin-server.js
+++ blpapi-plugin-server.js
@@ -1,10 +1,6 @@
 // Communication
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
+const server = require('http').Server();
 const io = require('socket.io')(server);
-app.use(express.static('./'));
-app.get('/', (req, res) => res.sendFile('app.html', {root: __dirname}));
 server.listen(5432, '0.0.0.0');
 
 // Plugin
@@ -12,32 +8,19 @@ server.listen(5432, '0.0.0.0');
 const blpapi = require('./blpapi-emulator');
 const sessionArgs = {serverHost: 'localhost', serverPort: 8194};
 const requestTopic = 'plugin-request';
-const responseTopic = 'plugin-response';
 const errorTopic = 'plugin-error';
-let sessions = [];
 
 io.on('connection', socket => {
+    let session = new blpapi.Session(sessionArgs);
+
     socket.on(requestTopic, m => {
-        if (m.functionName === 'newSession') {
-            let session = new blpapi.Session(sessionArgs);
-            session.id = m.id;
-            sessions.push(session);
-            return;
-        }
-
-        let session = sessions.find(s => s.id === m.id);
-
         if (m.functionName === 'on') {
-            session[m.functionName](m.event, d => socket.emit(responseTopic, Object.assign(m, {payload: d})));
+            session.on(m.event, d => socket.emit(m.event, Object.assign(m, {payload: d})));
         } else {
-            if (typeof session[m.functionName] === 'function') {
-                try {
-                    session[m.functionName].apply(session, m.args);
-                } catch(err) {
-                    socket.emit(errorTopic, Object.assign(m, {error: err.message}));
-                }
-            } else {
-                socket.emit(errorTopic, Object.assign(m, {error: new Error(`Function <${m.functionName}> is not defined`)}));
+            try {
+                session[m.functionName].apply(session, m.args);
+            } catch(err) {
+                socket.emit(errorTopic, Object.assign(m, {error: err.message}));
             }
         }
 
