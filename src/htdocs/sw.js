79946fdc2fa5a749bf75e68600654c3d97ff5342
Nicholas Kolba <nicholaskolba@Nicholass-MacBook-Pro-2.local>
2017-01-03 11:25:17 -0500

    service worker poc

---
+++ src/htdocs/sw.js
@@ -0,0 +1,42 @@
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('serviceworker-cache-polyfill.js');
+
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('hyperblotter-sw').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+        'css/styles.css',
+        'logging.js',
+        'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
+      ]);
+    })
+  );
+});
+
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
+self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
+  event.respondWith(
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
+    })
+  );
+});
